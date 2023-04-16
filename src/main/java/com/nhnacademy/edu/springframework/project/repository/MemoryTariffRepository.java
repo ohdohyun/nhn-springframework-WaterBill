dbb115c2ed9d41ccabe9bb19e483a98ec689146a
오도현 <ohdo123@naver.com>
2023-04-16 21:38:00 +0900

    파일명에 따라 Json Csv 선택하도록 수정

--- src/main/java/com/nhnacademy/edu/springframework/project/repository/MemoryTariffRepository.java
+++ src/main/java/com/nhnacademy/edu/springframework/project/repository/MemoryTariffRepository.java
@@ -2,6 +2,7 @@ package com.nhnacademy.edu.springframework.project.repository;
 
 import com.nhnacademy.edu.springframework.project.parser.DataParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -13,14 +14,19 @@ public class MemoryTariffRepository implements TariffRepository {
 
     List<Tariff> tariffs = new ArrayList<>();
 
-    private final DataParser dataParser;
+    private final DataParser csvDataParser;
+    private final DataParser jsonDataParser;
 
-    public MemoryTariffRepository(DataParser dataParser) {
-        this.dataParser = dataParser;
+    @Autowired
+    public MemoryTariffRepository(@Qualifier("csvDataParser") DataParser csvDataParser,
+                                  @Qualifier("jsonDataParser") DataParser jsonDataParser) {
+        this.csvDataParser = csvDataParser;
+        this.jsonDataParser = jsonDataParser;
     }
 
     @Override
     public void load(String filePath) {
+        DataParser dataParser = selectDataParser(filePath);
         this.tariffs = dataParser.parse(filePath);
     }
 
@@ -29,5 +35,15 @@ public class MemoryTariffRepository implements TariffRepository {
         return tariffs.stream()
                 .filter(t -> usage >= t.getStartRange() && usage <= t.getEndRange())
                 .collect(Collectors.toList());
+    }
+
+    private DataParser selectDataParser(String filePath) {
+        if (filePath.endsWith(".csv")) {
+            return csvDataParser;
+        } else if (filePath.endsWith(".json")) {
+            return jsonDataParser;
+        } else {
+            throw new IllegalArgumentException("Unsupported file format");
+        }
     }
 }
