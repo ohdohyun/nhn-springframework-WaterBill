6641d88ac734ab0a969b847ff06c8afe5ce9a141
오도현 <ohdo123@naver.com>
2023-04-16 19:36:51 +0900

    AOP logging (elapse.xml에 저장)

--- src/main/java/com/nhnacademy/edu/springframework/project/config/AppConfig.java
+++ src/main/java/com/nhnacademy/edu/springframework/project/config/AppConfig.java
@@ -2,8 +2,10 @@ package com.nhnacademy.edu.springframework.project.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan("com.nhnacademy.edu.springframework.project")
+@EnableAspectJAutoProxy
 public class AppConfig {
 }
---
+++ src/main/java/com/nhnacademy/edu/springframework/project/logging/LoggingAspect.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.edu.springframework.project.logging;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Around(value = "execution(* com.nhnacademy.edu.springframework.project..*(..))")
+    private Object loggingSendMessageTime(ProceedingJoinPoint pjp) {
+        StopWatch stopWatch = new StopWatch();
+        Object retVal;
+        try {
+            stopWatch.start();
+            retVal = pjp.proceed();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        } finally {
+            stopWatch.stop();
+            String className = pjp.getTarget().getClass().getSimpleName();
+            String methodName = pjp.getSignature().getName();
+            String elapsedTime = stopWatch.getTotalTimeMillis() + "ms";
+            log.info(className + "." + methodName + " " + elapsedTime);
+        }
+
+        return retVal;
+    }
+}
+
